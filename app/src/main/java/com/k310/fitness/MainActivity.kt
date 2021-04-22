53535ea15acaea40f683b030f5d487844dbee8c6
Hafshy Yazid Albisthami <hafshy@gmail.com>
2021-04-22 22:50:33 +0700

    Add navbar

--- app/src/main/java/com/k310/fitness/MainActivity.kt
+++ app/src/main/java/com/k310/fitness/MainActivity.kt
@@ -2,10 +2,42 @@ package com.k310.fitness
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
+import com.k310.fitness.databinding.ActivityMainBinding
+import com.k310.fitness.fragments.HistoryFragment
+import com.k310.fitness.fragments.NewsFragment
+import com.k310.fitness.fragments.ScheduleFragment
+import com.k310.fitness.fragments.TrainingFragment
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val newsFragment = NewsFragment()
+        val trainingFragment = TrainingFragment()
+        val historyFragment = HistoryFragment()
+        val scheduleFragment = ScheduleFragment()
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        makeCurrentFragment(newsFragment)
+        binding.bottomNavbar.setOnItemSelectedListener {
+            when(it) {
+                R.id.nav_news -> makeCurrentFragment(newsFragment)
+                R.id.nav_training -> makeCurrentFragment(trainingFragment)
+                R.id.nav_history -> makeCurrentFragment(historyFragment)
+                R.id.nav_schedule -> makeCurrentFragment(scheduleFragment)
+            }
+        }
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
 }
