008528aabab5a933e47984b284626879af6c39d8
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2021-04-28 17:36:42 +0700

    feat: add API class

---
+++ app/src/main/java/com/k310/fitness/api/Api.kt
@@ -0,0 +1,15 @@
+package com.k310.fitness.api
+
+import com.k310.fitness.models.NewsResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface Api {
+    @GET("top-headlines")
+    fun getAllNews(
+        @Query("country") country: String?,
+        @Query("category") category: String?,
+        @Query("apiKey") apiKey: String?
+    ): Call<NewsResponse?>?
+}
