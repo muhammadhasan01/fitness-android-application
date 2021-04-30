c6438bb397d04de6d6a2611068bc0112c06037aa
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2021-04-30 14:20:04 +0700

    feat: create News Item Click Listener

---
+++ app/src/main/java/com/k310/fitness/util/NewsItemClickListener.kt
@@ -0,0 +1,38 @@
+package com.k310.fitness.util
+
+import android.content.Context
+import android.view.GestureDetector
+import android.view.GestureDetector.SimpleOnGestureListener
+import android.view.MotionEvent
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.OnItemTouchListener
+
+class NewsItemClickListener(
+    context: Context?,
+    recyclerView: RecyclerView,
+    private val mListener: OnItemClickListener
+) : OnItemTouchListener {
+    interface OnItemClickListener {
+        fun onItemClick(view: View, position: Int)
+    }
+
+    private val mGestureDetector: GestureDetector =
+        GestureDetector(context, object : SimpleOnGestureListener() {
+            override fun onSingleTapUp(e: MotionEvent): Boolean {
+                return true
+            }
+        })
+
+    override fun onInterceptTouchEvent(view: RecyclerView, e: MotionEvent): Boolean {
+        val childView = view.findChildViewUnder(e.x, e.y)
+        if (childView != null && mGestureDetector.onTouchEvent(e)) {
+            mListener.onItemClick(childView, view.getChildLayoutPosition(childView))
+        }
+        return false
+    }
+
+    override fun onTouchEvent(view: RecyclerView, motionEvent: MotionEvent) {}
+    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
+
+}
