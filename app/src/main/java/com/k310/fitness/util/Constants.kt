6b479afcaafe9b15efe810b00798c2fa07e22911
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2021-04-30 14:20:57 +0700

    chore: add new constant extra message

--- app/src/main/java/com/k310/fitness/util/Constants.kt
+++ app/src/main/java/com/k310/fitness/util/Constants.kt
@@ -3,4 +3,5 @@ package com.k310.fitness.util
 object Constants {
     const val APP_DB_NAME = "fitness_k310_db"
     const val REQ_CODE_LOCATION_PERMISSIONS = 0
+    const val EXTRA_MESSAGE = "com.k310.fitness.MESSAGE"
 }
